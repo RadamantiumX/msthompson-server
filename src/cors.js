a33850bb2c7f5d56f0e6fbddcde4bb3e42fbaead
Eduardo Omar Arede <82120092+RadamantiumX@users.noreply.github.com>
2025-07-11 19:55:42 -0300

    feat: adding cors options

---
+++ src/cors.js
@@ -0,0 +1,9 @@
+export const corsOptions = {
+    origin:[
+        "http://localhost:5000",
+        "http://localhost:4000",
+        "http://localhost:3000",
+        "https://pruebas.msthompson.edu.ar/",
+        "https://pruebas.msthompson.edu.ar/*"
+    ]
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import informesRouter from './routes.js'
+import { corsOptions } from './cors.js'
 
 const app = express()
 const PORT = 4000
 app.use(bodyParser.urlencoded({ extended:true }))
 app.use(bodyParser.json())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.get("/", (req, res)=>{
     res.status(200).json({ message: "Server on" })
